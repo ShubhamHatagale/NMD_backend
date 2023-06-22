2a1163e5a1ff980f05b6b502f3fcc36617190183
ShubhamHatagale <shubhamhatagale02@gmail.com>
2023-06-22 10:53:01 +0530

    22Jun23

--- Main.php
+++ Main.php
@@ -4,11 +4,6 @@ require_once 'config.php';
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-// require_once 'path_to_openai_lib/openai-php/OpenAI.php';
-
-// use OpenAI\OpenAI;
-
-// OpenAI::setApiKey('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');
 
 
 class Main
@@ -86,51 +81,31 @@ class Main
             return false;
         }
     }
-
-
-    // public function chatgpt($message)
-    // {$response = OpenAI::complete([
-    //     'model' => 'gpt-3.5-turbo',
-    //     'prompt' => $message,
-    //     'maxTokens' => 100,
-    //     'temperature' => 0.6,
-    //     'n' => 1,
-    //     'stop' => null,
-    // ]);
-
-    // return $response['choices'][0]['text'];
-    // }
-
-    function chatgpt($message) {
-        $url = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-        $data = array(
-            'prompt' => $message,
-            'max_tokens' => 100,
-            'temperature' => 0.6,
-            'n' => 1
-        );
-        $headers = array(
-            'Content-Type: application/json',
-            'Authorization: Bearer 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'
-        );
     
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // getBy_latest_where_condition
+    public function getBy_latest_where_condition($db_column,$where_condition)
+    {
+        try {
+            $stmt = $this->conn->prepare("SELECT * FROM $db_column $where_condition");
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-        $response = curl_exec($ch);
-        curl_close($ch);
+            // Remove HTML tags from the fetched data
+            foreach ($data as &$item) {
+                foreach ($item as $key => &$value) {
+                    $value = strip_tags($value);
+                    $value = str_replace('&nbsp;', '', $value);
+
+                }
+            }
     
-        $json = json_decode($response, true);
-        return $json['choices'][0]['text'];
+            return $data;
+        } catch (PDOException $e) {
+            return false;
+        }
     }
-    
-    
-    
-    
-    
 
-    // Add other CRUD methods: create(), update(), delete()
+    
+    
+    
 }
--- api-projects.php
+++ api-projects.php
@@ -5,7 +5,21 @@ require_once 'Main.php';
 $Main = new Main();
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Fetch all blog records
-    $data = $Main->getBy_where_condition("projects","where `show`='yes'");
-    echo json_encode($data);
+    if (isset($_GET['function'])) {
+        $function = $_GET['function'];
+        switch ($function) {
+            case 'data':
+                $data = $Main->getBy_where_condition("projects", "where `show`='yes'");
+                echo json_encode($data);
+                break;
+            case 'latest_data':
+                $data = $Main->getBy_latest_where_condition("projects", "where `show`='yes' ORDER BY id DESC");
+                echo json_encode($data);
+                break;
+            default:
+                echo json_encode(array('error' => 'Invalid function specified'));
+        }
+    } else {
+        echo json_encode(array('error' => 'Function parameter not provided'));
+    }
 }
